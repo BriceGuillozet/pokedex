7f63df30b7b1d8078fbfc56092c7528500ac917a
Brice Guillozet <brice.guillozet@gmail.com>
2021-04-06 14:26:05 +0200

    add db url for heroku

--- app/database.js
+++ app/database.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 // On utilise pg ici, son constructeur va nous permettre de nous connecter à la BDD, on passe le lien psql contenu dans le .env
-const client = new Client(process.env.PG_URL);
+const client = new Client(process.env.DATABASE_URL);
 // On connecte le client
 client.connect();
 
