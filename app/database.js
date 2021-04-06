335d37ce9307581fa4a860367d209720e3114117
Brice Guillozet <brice.guillozet@gmail.com>
2021-04-06 18:27:03 +0200

    change connection to db for heroku

--- app/database.js
+++ app/database.js
@@ -1,6 +1,11 @@
 const { Client } = require("pg");
 // On utilise pg ici, son constructeur va nous permettre de nous connecter à la BDD, on passe le lien psql contenu dans le .env
-const client = new Client(process.env.DATABASE_URL);
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { 
+        rejectUnauthorized: false
+    }
+});
 // On connecte le client
 client.connect();
 
